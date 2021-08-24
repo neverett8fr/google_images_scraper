d3e28ecf71a8868618e28d15f4ffdda9b13217cf
Nathan Everett <nathan.everett@students.plymouth.ac.uk>
2021-08-24 23:54:30 +0100

    changed to use urllib -- and auto chooses file type

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-
-
-import requests
+import requests, urllib
 
 def all_img_urls(search_word, count):
     # this routine returns a list of all urls with images
@@ -10,12 +8,13 @@ def all_img_urls(search_word, count):
     j = 0
     while j < count:
         r = requests.get(
-            "https://www.google.com/search?q=" + search_word + "&tbm=isch" + "&num=3" + "&start=" + str(3*j))
+            "https://www.google.com/search?q=" + search_word + "&tbm=isch" + "&num=3" + "&start=" + str(3*j) + "&safe=off")
 
         for i, val in enumerate(str(r.text.replace(">", "").split("<")).split()):
             if "src=\"http" in val:
-                output.append(val.replace("src=\"", "").replace("\"/',", ""))
-                # print(val.replace("src=\"", "").replace("\"/',", ""))
+                   output.append(val.replace("src=\"", "").replace("\"/',", ""))
+                   # print(val.replace("src=\"", "").replace("\"/',", ""))
+
         j += 3
 
     return output
@@ -24,16 +23,20 @@ def download_img(url_array):
 
     for i, url in enumerate(url_array):
         # wb write binary
-        with open("images/" + str(i)+".jpg", "wb") as handle:
-            r = requests.get(url, stream=True)
+        #with open("images/" + "img_" + str(i)+".png", "wb") as handle:
+            #r = requests.get(url, stream=True)
+            #for block in r.iter_content(1024):
+                #if not block:
+                    #break
+                #handle.write(block)
 
-            for block in r.iter_content(1024):
-                if not block:
-                    break
+        r = requests.get(url)
+        urllib.request.urlretrieve(url, "images/img_"+str(i)+"."+r.headers.get("Content-Type").replace("image/", ""))
 
-                handle.write(block)
 
-search_par = input("Enter the images you want to search for:  ")
+
+search_par = input("Enter the images you want to search for:  ").replace(" ", "%20")
 search_num = input("Enter the amount of images you want:  ")
 
 download_img(all_img_urls(search_par, int(search_num)))
+#all_img_urls(search_par, int(search_num))
