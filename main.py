c607c0c470e8dc6c22715c191fe6edce32b846a6
Nathan Everett <nathan.everett@students.plymouth.ac.uk>
2021-08-25 02:02:36 +0100

    changed function to write image files as it goes along

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import requests, urllib
 def all_img_urls(search_word, count):
     # this routine returns a list of all urls with images
 
-    output = []
-
     j = 0
     while j < count:
         r = requests.get(
@@ -12,11 +10,22 @@ def all_img_urls(search_word, count):
 
         for i, val in enumerate(str(r.text.replace(">", "").split("<")).split()):
             if "src=\"http" in val:
-                output.append(val.replace("src=\"", "").replace("\"/',", ""))
-                   # print(val.replace("src=\"", "").replace("\"/',", ""))
+                # output.append(val.replace("src=\"", "").replace("\"/',", ""))
+                download_img_thumbnail_singular(val.replace("src=\"", "").replace("\"/',", ""), j)
         j += 1
 
-    return output
+
+def download_img_thumbnail_singular(url, number):
+    try:
+        with open("images/" + "img_" + number + ".jpg", "wb") as handle:
+            r = requests.get(url, stream=True)
+            for block in r.iter_content(1024):
+                if not block:
+                    break
+                handle.write(block)
+    except:
+        print("Error - Not found")
+
 
 def download_img_thumbnails(url_array):
 
@@ -44,6 +53,7 @@ search_par = input("Enter the images you want to search for:  ").replace(" ", "%
 search_num = input("Enter the amount of images you want:  ")
 search_type = input("Enter thumbnail(0) or image(1):  ")
 if search_type == "0":
-    download_img_thumbnails(all_img_urls(search_par, int(search_num)))
+    # download_img_thumbnails(all_img_urls(search_par, int(search_num)))
+    download_img_thumbnail_singular(all_img_urls(search_par, int(search_num)))
 
 #all_img_urls(search_par, int(search_num))
